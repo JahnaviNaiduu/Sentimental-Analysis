5f90a400a86fe3cb4e4dcfd8ead2c21908805111
unknown <jahnavig@example.com>
2025-09-17 22:33:59 +0530

    working model

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -13,6 +13,28 @@ import syllapy  #counts how many syllables are in words
 st.set_page_config(page_title="Sentiment Analysis-NLP", page_icon="📊", layout="wide") #browser tab
 st.title("Sentiment Analysis") #for webpage
 
+# Function to perform sentiment analysis on text
+def perform_sentiment_analysis(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+    word_count=len(blob.words)
+    complex_words=sum(1 for word in blob.words if len(word) > 3)
+    syllable_per_word = sum(syllapy.count(word) for word in blob.words) / word_count
+    personal_pronouns = blob.words.count("I") + blob.words.count("me") + blob.words.count("my") + blob.words.count("My")
+    return polarity, subjectivity,word_count,complex_words,syllable_per_word,personal_pronouns
+
+# Function to extract text from an image URL using pytesseract
+def extract_text_from_image_url(image_url):
+    try:
+        response = requests.get(image_url)
+        img = Image.open(BytesIO(response.content))
+        text = pytesseract.image_to_string(img)
+        return text
+    except Exception as e:
+        print(f"Error extracting text from the image URL. Error details: {e}")
+        return None
+
 df = pd.read_csv("SearchEngine_Dataset.csv", encoding='latin1').fillna("") #encoding=“Read special letters correctly.”
 
 # Keyword recommendations for dropdown
@@ -58,8 +80,30 @@ with cols[n_row % N_cards_per_row]:
 if st.button(f"Analyse_{n_row}"):
     url = row['URL']
 
-
-
+    if url.endswith(('jpg', 'jpeg', 'png', 'gif')):
+                extracted_text = extract_text_from_image_url(url)
+                if extracted_text:
+                    polarity, subjectivity,word_counts,complex_word,syllable,personal = perform_sentiment_analysis(extracted_text)
+                    #st.write("Article Text:", text)
+                    st.write("Polarity:", format(polarity,".2f"))
+                    st.write("Subjectivity:", format(subjectivity,".2f"))
+                    df=pd.DataFrame([polarity,subjectivity,word_counts,complex_word,syllable,personal])
+                    df.index = ['polarity', 'subjectivity', 'word_counts', 'complex_word', 'syllable', 'personal']
+                    st.bar_chart(df)
+                else:
+                    st.write("Text extraction failed. Please check the image URL and try again.")
+    else:
+                article = newspaper.Article(url)
+                article.download()
+                article.parse()
+                text = article.text
+                polarity, subjectivity, word_counts, complex_word, syllable, personal = perform_sentiment_analysis(text)
+                #st.write("Article Text:", text)
+                st.write("Polarity:", format(polarity,".2f"))
+                st.write("Subjectivity:", format(subjectivity,".2f"))
+                df = pd.DataFrame([polarity, subjectivity, word_counts, complex_word, syllable, personal])
+                df.index=['polarity', 'subjectivity', 'word_counts', 'complex_word', 'syllable', 'personal']
+                st.bar_chart(df)
 
 
 
